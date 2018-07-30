274607d45ad8be3fb1673eef0057c64ea5ac59e7
Daniel W. Lu <dandydanny@gmail.com>
2018-07-30 01:09:14 -0400

    Added logic to hide nav links

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-console.log("hello world!");
+// Toggle nav links visiblity after clicking on hamburger
 $( "#hamburger" ).click(function() {
+    console.log("clicked");
+    $(".link").toggleClass("show");
+});
+
+// Hide nav links after clicking on them
+$( ".link" ).click(function() {
     console.log("clicked");
     $(".link").toggleClass("show");
 });
