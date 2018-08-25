d205ee3c4b615a34959997dc502416fc501be4b9
Daniel W. Lu <dandydanny@gmail.com>
2018-08-25 15:55:53 -0400

    Added event listener to pause other audio players

--- js/script.js
+++ js/script.js
@@ -9,3 +9,13 @@ $( ".link" ).click(function() {
     console.log("clicked");
     $(".link").removeClass("show");
 });
+
+// Pause other players when playing current audio
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
